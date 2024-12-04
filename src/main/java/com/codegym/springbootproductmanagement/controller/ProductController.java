dd40e2cbab50bce6bbdacc9c286f50bd18a902b7
TungNK01 <tung.nguyen@codegym.vn>
2024-12-04 10:13:00 +0700

    case 1

---
+++ src/main/java/com/codegym/springbootproductmanagement/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.codegym.springbootproductmanagement.controller;
+
+import com.codegym.springbootproductmanagement.model.Product;
+import com.codegym.springbootproductmanagement.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private IProductService productService;
+
+    @GetMapping("/create")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/product/create");
+        modelAndView.addObject("product", new Product());
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView saveProduct(@ModelAttribute("product") Product product) {
+        productService.save(product);
+        ModelAndView modelAndView = new ModelAndView("/product/create");
+        modelAndView.addObject("product", new Product());
+        return modelAndView;
+    }
+}
